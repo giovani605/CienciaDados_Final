fce1a3edc5f4e35266d395520eac258f8ec414a4
Giovani <giovanibossoni@hotmail.com>
2018-06-16 00:09:33 -0300

    Implementado a extração dos stats do player na partida

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import riotwatcher
 
+def processar(dados):
+    ## chegando aqui cosnegui pogar o stat do meu player
+    print(dados)
+
+
+
 arq = open("key.txt")
 
 key = arq.readline()
@@ -9,8 +15,19 @@ perfil = watcher.summoner.by_name(region="br1",summoner_name="GuaxininAtomico")
 
 
 myId = perfil["accountId"]
+myIdSum = perfil["id"]
+myNick = perfil["name"]
 
 partidas = watcher.match.matchlist_by_account(region="br1", account_id=myId)
 
 for match in partidas["matches"]:
-    print(match)
+    partida = watcher.match.by_id(region="br1", match_id=match["gameId"])
+    for participante in partida["participantIdentities"]:
+            if participante["player"]["summonerId"] == myIdSum:
+                participanteId = participante["participantId"]
+                for dados in partida["participants"]:
+                    if dados["participantId"] is participanteId:
+                        processar(dados)
+
+
+
