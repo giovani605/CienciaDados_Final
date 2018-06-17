6955fd1ce699ceb9998c20091569ea1ab4562f06
Giovani <giovanibossoni@hotmail.com>
2018-06-17 10:26:00 -0300

    implementado leitura das partidas salvas e corelação com o rank commitando tbm o zip dos dados

--- lerDados.py
+++ lerDados.py
@@ -5,6 +5,18 @@ print(dict)
 dados = open("dados2.csv",'r')
 csv_reader = csv.DictReader(dados)
 fields = ['accountId','matchId','visionScore']
+rankPlayers = open('listPlayers.csv','r')
+rank_csv = csv.DictReader(rankPlayers)
+print(rank_csv.fieldnames)
+# tenho que colocar o resultado do csv do rank em um dicionario pq vou consultar ele muitas vezes
+lista = list(rank_csv)
+
+def procurarRank(contaId):
+    for entry in lista:
+        if entry["summonerId"] == contaId:
+            return entry
+    print("nao encontrei")
+
 
 
 for match in csv_reader:
@@ -12,4 +24,7 @@ for match in csv_reader:
     arquivo = open("dados/"+str(match["matchId"])+".pkl", 'rb')
     dict = pickle.load(arquivo)
     ## a variavel dict eh aquele json match
+    ## o match account id eh na vdd o summoner id
+    rank = procurarRank(match["accountId"])
+    print(rank)
     print(dict)
