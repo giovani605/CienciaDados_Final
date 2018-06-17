f44a1fe5d05c47e4acfd0993b21b4bfacf91b0ef
Giovani <giovanibossoni@hotmail.com>
2018-06-17 17:54:15 -0300

    Implementando funcao que salva uma linha no csv Refatorando variaveis para faciltar leitura

--- lerDados.py
+++ lerDados.py
@@ -3,7 +3,7 @@ import csv
 
 print(dict)
 dados = open("dados2.csv",'r')
-csv_reader = csv.DictReader(dados)
+csv_partidas = csv.DictReader(dados)
 fields = ['accountId','matchId','visionScore']
 rankPlayers = open('listPlayers.csv','r')
 rank_csv = csv.DictReader(rankPlayers)
@@ -18,19 +18,35 @@ def procurarRank(contaId):
     print("nao encontrei")
 
 
+fields = ['summonerId','accountId','tier','rank','matchId']
+tabela = open('saida.csv', 'w')
+csv_writer = csv.DictWriter(tabela, fieldnames=fields, delimiter=',')
+csv_writer.writeheader()
 
-for match in csv_reader:
+def processar(player,matchId):
+    ## chegando aqui cosnegui pogar o stat do meu player
+    linha = {}
+    linha[fields[0]] = player['summonerId']
+    linha[fields[1]] = player['accountId']
+    linha[fields[2]] = player['tier']
+    linha[fields[3]] = player['rank']
+    linha[fields[4]] = matchId
+    print("salvei")
+    print(linha)
+    csv_writer.writerow(linha)
+
+for match in csv_partidas:
     ## carregar o dado do match inteiro
-    arquivo = open("dados/"+str(match["matchId"])+".pkl", 'rb')
+    arquivo = open("dados/" + str(match["matchId"]) + ".pkl", 'rb')
     partida = pickle.load(arquivo)
     ## a variavel dict eh aquele json match
     ## o match account id eh na vdd o summoner id
-    rank = procurarRank(match["accountId"])
+    playerRank = procurarRank(match["accountId"])
     for participante in partida["participantIdentities"]:
         teste = participante["player"]
         if str(participante["player"]["summonerId"]) == match["accountId"]:
             participanteId = participante["participantId"]
             for dados in partida["participants"]:
                 if dados["participantId"] is participanteId:
-                    ## chegamos nos dados do nosso alvo
+                    processar(playerRank, match["matchId"])
                     print("cheguei")
